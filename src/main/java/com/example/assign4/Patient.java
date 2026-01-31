594d545f4d119db18b02131456a06a492f519d15
Madi Melis <250270@astanait.edu.kz>
2026-01-31 21:28:57 +0500

    Add getters and setters to Patient

--- src/main/java/com/example/assign4/Patient.java
+++ src/main/java/com/example/assign4/Patient.java
@@ -1,4 +1,58 @@
 package com.example.assign4;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "patient")
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private int age;
+    private int doctorId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctor(int doctorId) {
+        this.doctorId = doctorId;
+    }
 }
